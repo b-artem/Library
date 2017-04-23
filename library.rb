dd0796974469fca328065029e2415b7dea442153
b-artem <ba.artyom@gmail.com>
2017-04-23 19:37:20 +0300

    Add top book method

--- library.rb
+++ library.rb
@@ -1,116 +1,133 @@
 require 'yaml'
 
-  class Book
-    attr_reader :title, :author
-    def initialize(title, author)
-      @title = title
-      @author = author
+class Book
+  attr_reader :title, :author
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+end
+
+class Order
+  attr_reader :book, :reader, :date
+  def initialize(book, reader, date)
+    @book = book
+    @reader = reader
+    @date = date
+  end
+end
+
+class Reader
+  attr_reader :name, :email, :city, :street, :house
+  def initialize(name, email, city, street, house)
+    @name = name
+    @email = email
+    @city = city
+    @street = street
+    @house = house
+  end
+end
+
+class Author
+  attr_reader :name, :biography
+  def initialize(name, biography)
+    @name = name
+    @biography = biography
+  end
+end
+
+class Library
+
+  def initialize(name)
+    @name = name
+  end
+
+  def get_data(file)
+    puts "Reading library data from file #{file}"
+    @books = YAML.load_file(file)
+    # puts file.closed?
+  end
+
+  def save_data(file)
+    puts "Saving library data to file #{file}  ..."
+    f = File.new(file, 'w')
+    f.write(to_yaml)
+    f.close
+  end
+
+  def generate_orders(orders_amount)
+    @orders = []
+    orders_amount.times do
+      book_index = rand(0...@books.count)
+      reader_index = rand(0...@readers.count)
+      date = Date.today - rand(1..30)
+      @orders << Order.new(@books[book_index].title, @readers[reader_index].name, date)
+    end
+    puts "Generating #{orders_amount} orders..."
+  end
+
+  def top_reader
+    if @orders
+      readers = Hash.new(0)
+      @orders.each { |order| readers[order.reader] += 1 }
+      max = readers.values.max
+      top_reader = readers.select { |_, value| value == max }
+      puts top_reader
+    else
+      puts "There is no orders in the library yet. You may create them using
+            generate_orders(orders_amount) method"
     end
   end
 
-  class Order
-    attr_reader :book, :reader, :date
-    def initialize(book, reader, date)
-      @book = book
-      @reader = reader
-      @date = date
+  def top_book
+    if @books
+      books = Hash.new(0)
+      @orders.each { |order| books[order.book] += 1 }
+      max = books.values.max
+      top_book = books.select { |_, value| value == max }
+    else
+      puts "There is no orders in the library yet. You may create them using
+      generate_orders(orders_amount) method"
     end
   end
 
-  class Reader
-    attr_reader :name, :email, :city, :street, :house
-    def initialize(name, email, city, street, house)
-      @name = name
-      @email = email
-      @city = city
-      @street = street
-      @house = house
+  # def top3_books
+  #   top3_books = []
+  #   3.times
+  #   top_book
+
+  # end
+
+  # Utils for reading data from TXT files ######################################
+  def get_books_data(file)
+    @books = []
+    f = File.open(file)
+    f.each do |line|
+      params = line.split(',').map(&:strip)
+      @books << Book.new(params[0], params[1])
     end
+    f.close
   end
 
-  class Author
-    attr_reader :name, :biography
-    def initialize(name, biography)
-      @name = name
-      @biography = biography
+  def get_readers_data(file)
+    @readers = []
+    f = File.open(file)
+    f.each do |line|
+      params = line.split(',').map(&:strip)
+      @readers << Reader.new(params[0], params[1], params[2], params[3], params[4])
     end
+    f.close
   end
 
-  class Library
-
-    def initialize(name)
-      @name = name
+  def get_authors_data(file)
+    @authors = []
+    f = File.open(file)
+    f.each do |line|
+      params = line.split(',').map(&:strip)
+      @authors << Author.new(params[1], params[0])
     end
-
-    def get_data(file)
-      @books = YAML.load_file(file)
-      # puts "#{@books[9].title}, #{@books[9].author}"
-    end
-
-    def save_data(file)
-      puts 'Saving library data to file...'
-      f = File.new(file, 'w')
-      f.write(self.to_yaml)
-      f.close
-      puts 'File closed?: ' + "#{f.closed?}"
-    end
-
-    def generate_orders(orders_amount)
-      @orders = []
-      prng = Random.new
-      orders_amount.times do
-        book_index = prng.rand(0...@books.count)
-        reader_index = prng.rand(0...@readers.count)
-        date = Date.today - prng.rand(1..30)
-        @orders << Order.new(@books[book_index].title, @readers[reader_index].name, date)
-      end
-    end
-
-    def top_reader
-      if @orders
-        readers = Hash.new(0)
-        @orders.each { |order| readers[order.reader] += 1 }
-        max = readers.values.max
-        top_reader = readers.select { |_, value| value == max }
-        puts "Top reader(s): #{top_reader}"
-      else
-        puts "There is no orders in the library yet. You may create them using
-              generate_orders(orders_amount) function"
-      end
-    end
-
-############################################################################
-    def get_books_data(file)
-      @books = []
-      f = File.open(file)
-      f.each do |line|
-        params = line.split(',').map(&:strip)
-        @books << Book.new(params[0], params[1])
-      end
-      f.close
-      @books.each { |book| puts "#{book.title}, #{book.author}" }
-    end
-
-    def get_readers_data(file)
-      @readers = []
-      f = File.open(file)
-      f.each do |line|
-        params = line.split(',').map(&:strip)
-        @readers << Reader.new(params[0], params[1], params[2], params[3], params[4])
-      end
-      f.close
-    end
-
-    def get_authors_data(file)
-      @authors = []
-      f = File.open(file)
-      f.each do |line|
-        params = line.split(',').map(&:strip)
-        @authors << Author.new(params[1], params[0])
-      end
-      f.close
-    end
-
-#############################################################################
-
+    f.close
   end
+  ###########################################################################
+
+end
--- main.rb
+++ main.rb
@@ -2,19 +2,17 @@ $LOAD_PATH << File.expand_path(File.dirname(__FILE__))
 
 require 'library.rb'
 
-puts 'Begin...'
-
 library = Library.new('Foreign Literature')
 path = File.expand_path(File.dirname(__FILE__) + '/db')
 
 library.get_books_data(path + '/books.txt')
 library.get_readers_data(path + '/readers.txt')
 library.get_authors_data(path + '/authors.txt')
+# library.get_data(path + '/library_saved.yml')
 
-library.generate_orders(20)
+library.generate_orders(100)
 
 library.top_reader
+puts library.top_book
 
 library.save_data(path + '/library_saved.yml')
-
-puts 'End'
